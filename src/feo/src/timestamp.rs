7098743604708972cec60a21e308e02602708a15
Artem Shein <artem.shein@accenture.com>
2026-02-18 14:04:23 +0100

    Fix sources move build issues

--- src/feo/src/timestamp.rs
+++ src/feo/src/timestamp.rs
@@ -1,15 +1,15 @@
-/********************************************************************************
- * Copyright (c) 2025 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
+// *******************************************************************************
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// SPDX-License-Identifier: Apache-2.0
+// *******************************************************************************
 
 //! Timestamping module
 
@@ -146,7 +146,7 @@ pub struct SyncInfo {
     since_epoch: core::time::Duration,
 }
 
-/// Return current system time and instant as a TimeInfo object  
+/// Return current system time and instant as a TimeInfo object
 fn time_info_now() -> TimeInfo {
     let mut tries_remaining: i32 = MAX_TRIES;
     loop {
